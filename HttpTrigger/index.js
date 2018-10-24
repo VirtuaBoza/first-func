d5ee842802f5804b0228948e1f498871b37ef919
Andrew Boza <andrew.m.boza@gmail.com>
2018-10-24 06:03:03 -0400

    Returns json

--- HttpTrigger/index.js
+++ HttpTrigger/index.js
@@ -4,7 +4,8 @@ module.exports = async function(context, req) {
   if (req.query.name || (req.body && req.body.name)) {
     context.res = {
       // status: 200, /* Defaults to 200 */
-      body: 'Hello ' + (req.query.name || req.body.name),
+      body: { greeting: 'Hello ' + (req.query.name || req.body.name) },
+      headers: { 'Content-Type': 'application/json' },
     };
   } else {
     context.res = {
